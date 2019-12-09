aabb591496c2837737aaf6647b0ea2274a26741e
sandycamilo <scam499@lasierra.edu>
2019-12-09 13:34:23 +0100

    hero

--- superheroes.py
+++ superheroes.py
@@ -54,6 +54,185 @@ class Hero:
         else:
             True
 
+    def fight(self, opponent):  
+        ''' Current Hero will take turns fighting the opponent hero passed in.
+        '''
+        
+        if len(self.abilities) == 0 and len(opponent.abilities) == 0:
+            print("Draw")
+        else:
+            while self.is_alive() == True and opponent.is_alive() == True:
+                opponent.take_damage(self.attack())
+                self.take_damage(opponent.attack())
+            
+            if self.current_health > opponent.current_health:
+                print("Winner:", self.name)
+                self.add_kill(1)
+                opponent.add_death(1)
+            else:
+                print("Winner:", opponent.name)
+                self.add_death(1)
+                opponent.add_kill(1)
+
+    def add_weapon(self, weapon):
+        '''Add weapon to self.abilities'''
+
+        self.abilities.append(weapon)
+
+    def add_kill(self, num_kills):
+        ''' Update self.kills by num_kills amount'''
+        self.kills += num_kills    
+
+    def add_death(self, num_deaths):
+        ''' Update deaths with num_deaths'''
+        self.deaths += num_deaths
+
+class Weapon(Ability):
+    def attack(self):
+        """  This method returns a random value
+        between one half to the full attack power of the weapon.
+        """
+        weapons_attack = self.max_damage // 2
+        return random.randint(weapons_attack, self.max_damage)
+
+class Team:
+    def __init__(self, name):
+        ''' Initialize your team with its team name and an empty list of heroes
+        '''
+        self.name = name
+        self.heroes = list()
+
+    def remove_hero(self, name):
+        '''Remove hero from heroes list.
+        If Hero isn't found return 0.
+        '''
+        foundHero = False
+        for hero in self.heroes:
+            if hero.name == name:
+                self.heroes.remove(hero)
+                foundHero = True
+        if not foundHero:
+            return 0
+
+    def view_all_heroes(self):
+        '''Prints out all heroes to the console.'''
+       
+        for hero in self.heroes:
+            print(hero.name)
+
+    def add_hero(self, hero):
+        '''Add Hero object to self.heroes.'''
+
+        self.heroes.append(hero)
+
+    def stats(self):
+        '''Print team statistics'''
+        for hero in self.heroes:
+            kd = hero.kills / hero.deaths
+            print("{} Kill/Deaths:{}".format(hero.name,kd))
+
+    def revive_heroes(self):
+        ''' Reset all heroes health to starting_health'''
+      
+        for hero in self.heroes:
+            hero.current_health = hero.starting_health
+
+    def attack(self, other_team):
+        ''' Battle each team against each other.'''
+
+        living_heroes = list()
+        living_opponents = list()
+
+        for hero in self.heroes:
+            living_heroes.append(hero)
+
+        for hero in other_team.heroes:
+            living_opponents.append(hero)
+
+        while len(living_heroes) > 0 and len(living_opponents)> 0:
+            hero = random.choice(living_heroes)
+            opponent = random.choice(living_opponents)
+            
+            return hero.fight(opponent)
+
+class Arena:
+    def __init__(self):
+        '''Instantiate properties
+            team_one: None
+            team_two: None
+        ''' 
+        self.team_one = list()
+        self.team_two = list()         
+    
+    def create_ability(self):
+        '''Prompt for Ability information.
+            return Ability with values from user Input
+        '''
+        name = input("What is the ability name?  ")
+        max_damage = input(
+            "What is the max damage of the ability?  ")
+
+        return Ability(name, max_damage)
+
+    def create_weapon(self):
+        '''Prompt user for Weapon information
+            return Weapon with values from user input.
+        '''
+        weapon = input(
+            "create a new weapon object")
+        max_damage = input(
+            "What is the max damage of the weapon?  ")
+        return Weapon(weapon, max_damage)
+
+    def create_armor(self):
+        '''Prompt user for Armor information
+          return Armor with values from user input.
+        '''
+        new_armor = input(
+            "create a new armor object")
+        max_block = input(
+            "What is the max block of the armor?")
+        return Armor(new_armor,max_block)
+
+    def create_hero(self):
+        '''Prompt user for Hero information
+          return Hero with values from user input.
+        '''
+        hero_name = input("Hero's name: ")
+        hero = Hero(hero_name)
+        add_item = None
+        while add_item != "4":
+           add_item = input("[1] Add ability\n[2] Add weapon\n[3] Add armor\n[4] Done adding items\n\nYour choice: ")
+           if add_item == "1":
+               ability = self.create_ability()
+               hero.add_ability(ability)
+           elif add_item == "2":
+               weapon = self.create_weapon()
+               hero.add_weapon(weapon)
+           elif add_item == "3":
+               armor = self.create_armor()
+               hero.add_armor(armor)
+        return hero
+
+    def build_team_one(self):
+        name = input("\nWhat is the name of Team 1? ")
+        self.team_one_size = input("How many heros do you want on your first team?  ")
+        self.team_one = Team(name)
+        for _ in range(int(self.team_one_size)):
+            hero = self.create_hero()
+            self.team_one.add_hero(hero)
+
+    def build_team_two(self):
+        name = input("\nWhat is the name of Team 2? ")
+        self.team_two_size = input("How many heros do you want on your second team?  ")
+        self.team_two = Team(name)
+        for _ in range(int(self.team_two_size)):
+            hero = self.create_hero()
+            self.team_two.add_hero(hero)
+
+    def team_battle(self):
+        '''Battle team_one and team_two together.'''
+        self.team_one.attack(self.team_two)
 
 if __name__ == "__main__":
     # If you run this file from the terminal
@@ -62,11 +241,3 @@ if __name__ == "__main__":
     hero = Hero("Grace Hopper", 200)
     hero.add_ability(ability)
     print(hero.abilities)
-
-
-if __name__ == "__main__":
-    # If you run this file from the terminal
-    # this block is executed.
-    ability = Ability("Debugging Ability", 20)
-    print(ability.name)
-    print(ability.attack())
